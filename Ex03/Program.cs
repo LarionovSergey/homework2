a3f012776228aec558b1d560e9d6489e10836894
Sergey L <laruonhik@yandex.ru>
2022-12-03 03:17:03 +0700

    Задача 3

---
+++ Ex03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <= 5)
+{
+    Console.WriteLine("Будний день");
+}
+else
+{
+    Console.WriteLine("Выходной");
+}
