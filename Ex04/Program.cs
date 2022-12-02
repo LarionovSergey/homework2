9501806328702c022c7a696b62465d4ee1cfb25b
Sergey L <laruonhik@yandex.ru>
2022-12-03 03:34:44 +0700

    Задача 4

---
+++ Ex04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a / b == b | b / a == a)
+{
+    Console.WriteLine ("Одно число является квадратом другого");
+}
+else
+{
+    Console.WriteLine("Одно число не является квадратом другого");
+}
