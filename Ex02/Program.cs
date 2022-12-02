124047e93ba72828cae5f6d2bef6aefb4643b6ca
Sergey L <laruonhik@yandex.ru>
2022-12-03 03:15:53 +0700

    Задача 2

---
+++ Ex02/Program.cs
@@ -0,0 +1,9 @@
+﻿Random random = new Random();  
+int number = 0;
+number = random.Next(1,9999);
+Console.WriteLine(number);
+string stringNumber = Convert.ToString(number);
+if (number > 100)
+Console.WriteLine("Третье число: " + stringNumber[2]);
+if (number < 100)
+Console.WriteLine("Третьей цифры нет");
